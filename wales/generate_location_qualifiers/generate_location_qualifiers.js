06f4e371b1349418c3d1d035bdcac17b524bc6e6
KacperSzyf <kacper.szyf@icloud.com>
2022-02-21 12:57:35 +0000

    renamed the function

--- wales/generate_location_qualifiers/generate_location_qualifiers.js
+++ wales/generate_location_qualifiers/generate_location_qualifiers.js
@@ -3,18 +3,19 @@ define(['jquery',
     'viewmodels/function',
     'bindings/chosen', 'arches'],
 function($, ko, FunctionViewModel, chosen, arches) {
-    return ko.components.register('views/components/functions/generate_KMSQ', {
+    return ko.components.register('views/components/functions/generate_location_qualifiers', {
         viewModel: function(params) {
             FunctionViewModel.apply(this, arguments);
             let self = this
             //Params ---
             this.triggering_nodegroups = params.config.triggering_nodegroups;
             //end params
+            //TODO: Change UUID's to live server ones
             this.triggering_nodegroups(['f58199ea-8ff9-11ec-9340-00155d9326d1'])
         }//5844718c-7dcc-11ec-a910-00155db3508e
         ,
         template: {
-            require: 'text!templates/views/components/functions/generate_KMSQ.htm'
+            require: 'text!templates/views/components/functions/generate_location_qualifiers.htm'
 
         }
     });
